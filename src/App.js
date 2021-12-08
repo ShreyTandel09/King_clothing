354d1696939d3924d556e82511b5a3ba2e79fb85
shrey <shreytandel09@gmail.com>
2021-12-08 17:13:08 +0530

    sign in page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component";
 import HomePage from "./pages/homepage/homepage.component";
 import Header from "./components/header/header.component";
+import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route exact element={<HomePage />} path="/" />
         <Route element={<ShopPage />} path="/shop" />
+        <Route element={<SignInAndSignUp />} path="/signin" />
       </Routes>
     </div>
   );
