d008d98e1f0530af12de6484e46558225f2c6677
shrey <shreytandel09@gmail.com>
2021-12-08 14:56:56 +0530

    styling the collection-preview and shop page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React from "react";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-
+import ShopPage from "./pages/shop/shop.component";
 import HomePage from "./pages/homepage/homepage.component";
-
-const HatsPage = () => (
-  <div>
-    <h1>HAts Page</h1>
-  </div>
-);
 
 function App() {
   return (
     <Routes>
       <Route exact element={<HomePage />} path="/" />
-      <Route element={<HatsPage />} path="/hats" />
+      <Route element={<ShopPage />} path="/shop" />
     </Routes>
   );
 }
