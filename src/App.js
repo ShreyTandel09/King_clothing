5df998a095150ac00eb5b0816e42c14e1f0d8559
shrey <shreytandel09@gmail.com>
2021-12-08 15:16:50 +0530

    done with header and links to home

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component";
 import HomePage from "./pages/homepage/homepage.component";
+import Header from "./components/header/header.component";
 
 function App() {
   return (
-    <Routes>
-      <Route exact element={<HomePage />} path="/" />
-      <Route element={<ShopPage />} path="/shop" />
-    </Routes>
+    <div>
+      <Header />
+
+      <Routes>
+        <Route exact element={<HomePage />} path="/" />
+        <Route element={<ShopPage />} path="/shop" />
+      </Routes>
+    </div>
   );
 }
 
